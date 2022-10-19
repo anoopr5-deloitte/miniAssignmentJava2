c0a11ef7db92dd5b6e4d2916f6c48e890dfb8247
Anoop <anoopr5@deloitte.com>
2022-10-19 17:24:25 +0530

    Array java file code changes

--- src/array.java
+++ src/array.java
@@ -1,28 +1,21 @@
 import java.util.Random;
 public class array {
     public static void main(String[] args){
-        int arr[],min=99999,max=0;
+        int arr[],min=101,max=0,i;
         arr =new int[10];
         Random random=new Random();
-        for (int i=0;i<10;i++)
+        System.out.print("\nGenerated Array : [ ");
+        for (i=0;i<10;i++)
         {
             arr[i]= random.nextInt(101);
-        }
-        for (int i=0;i<10;i++)
-        {
+            System.out.print(arr[i] + " ");
             if(min>arr[i])
                 min=arr[i];
-        }
-        for (int i=0;i<10;i++)
-        {
             if(max<arr[i])
                 max=arr[i];
         }
-        System.out.println("Maximum in the array is="+max);
-        System.out.println("Minimum in the array is="+min);
-
+        System.out.print("]\n");
+        System.out.println("Maximum in the array = "+max);
+        System.out.println("Minimum in the array = "+min);
     }
-
-
-
 }
