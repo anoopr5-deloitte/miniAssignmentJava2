0108e7047c944f949e0ffaac7fe1dad179d10c35
Anoop <anoopr5@deloitte.com>
2022-10-20 13:08:52 +0530

    changed

--- src/duplicateArrayitem.java
+++ src/duplicateArrayitem.java
@@ -2,13 +2,19 @@ public class duplicateArrayitem {
     public static void main(String [] args)
     {
         String arr[]={"java","anoop","javahungry","anoop","java"};
-
+        String result[]=new String[10];
         for (int i=0;i<arr.length;i++)
         {
             for (int j=i+1;j<arr.length;j++)
             {
                 if(arr[i].equals(arr[j]))
+                {
                     System.out.println(arr[i]);
+
+                }
+
+
+
             }
         }
 
--- src/sortZeros.java
+++ src/sortZeros.java
@@ -1,38 +1,26 @@
-import java.util.Arrays;
+
+import java.util.*;
 public class sortZeros {
     public static void main(String[] args){
 
         int[] arr = new int[15];
         arr = new int[]{1, 2, 4, 6, 0, 3, 0, 4, 5, 6, 0, 7, 0};
         int i, j = 0, k = 0;
-        int zero[]=new int[4];
-        int nonZero[]=new int [9];
-        int result[]=new int [13];
+        List<Integer> zero = new ArrayList<Integer>();
+        List<Integer> nonZero = new ArrayList<Integer>();
+        List<Integer> result = new ArrayList<Integer>();
+
 
         for (i = 0; i < arr.length; i++) {
             if (arr[i] == 0)
-                zero[j++] = arr[i];
+                zero.add(arr[i]);
             else
-                nonZero[k++]=arr[i];
+                nonZero.add(arr[i]);
         }
-        for (i = 0; i < zero.length; i++) {
-            System.out.print( zero[i]+" ");
-        }System.out.println("\n");
-        for (i = 0; i < nonZero.length; i++) {
-            System.out.print(nonZero[i]+" ");
-        }
-        System.out.println("\n");
-        Arrays.sort(nonZero);
-        for (i = 0; i < nonZero.length; i++) {
-            System.out.print(nonZero[i]+" ");
-        }
-        System.out.println("\n");
-        System.arraycopy(nonZero, 0, result, 0, nonZero.length);
-        System.arraycopy(zero, 0, result, nonZero.length, zero.length);
+        nonZero.sort(Comparator.naturalOrder());
 
-        for (i = 0; i < result.length; i++) {
-            System.out.print(+result[i]+" ");
-        }
+        nonZero.addAll(zero);
+        System.out.println(nonZero);
     }
 
 }
