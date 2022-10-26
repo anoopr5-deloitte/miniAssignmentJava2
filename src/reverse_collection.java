cede8747c8e29c1ea179cbef54e6f94cdb3dec18
Anoop <anoopr5@deloitte.com>
2022-10-26 15:08:26 +0530

    modified

--- src/reverse_collection.java
+++ src/reverse_collection.java
@@ -1,34 +1,33 @@
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.List;
-import java.util.*;
-public class reverse_collection {
-    public static void main(String[]args){
 
-        ArrayList<String> lst = new ArrayList<String>();
-        ArrayList<String> result = new ArrayList<String>();
-        lst.add("HashedinByDeloitte");
-        char temp; int last;
-        System.out.println(lst);
+class reverse_collection
+{
+    public static String reverse(String str)
+    {
 
-/*        last=lst.size();
-        for(int i=0;i<lst.size()/2;i++)
-        {
-            temp= lst[i];
-            lst[i].add(lst[last]);
-            lst[last]=temp;
+        if (str == null || str.equals("")) {
+            return str;
+        }
+        List<Character> list = new ArrayList<Character>();
+        for (char c: str.toCharArray()) {
+            list.add(c);
+        }
+        Collections.reverse(list);
+        StringBuilder builder = new StringBuilder(list.size());
+        for (Character c: list) {
+            builder.append(c);
         }
 
+        return builder.toString();
+    }
 
-        for(int i=lst.size()-1;i>=0;i--)
-        {
-           System.out.println(lst.subList(i,i+1));
-           //result.add(lst.get(i));
-        }
-        */
-        Collections.reverse(lst);
-        System.out.println(lst);
+    public static void main(String[] args)
+    {
+        String str = "Hashedin by deloitte";
+        str = reverse(str);
+
+        System.out.println("The reverse of the given string is: " + str);
     }
 }
